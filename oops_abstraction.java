8d1c7ffe51e85fe1d1603315ad010b3a66720806
Sujal Shree <149189322+stonecoldshree@users.noreply.github.com>
2024-08-25 10:15:47 +0530

    Add files via upload

---
+++ oops_abstraction.java
@@ -0,0 +1,59 @@
+import java.util.*;
+abstract class shape
+{
+    int l,b;
+    int ar=0;
+    int peri=0;
+    shape()
+    {
+        System.out.println("Welcome all...");
+    }
+    abstract public void area();
+    abstract public void perimeter();
+}
+class square extends shape
+{
+    public void area()
+    {
+        ar=l*l;
+        System.out.println("Area is: "+ar);
+    }
+    public void perimeter()
+    {
+        peri=4*l;
+        System.out.println("Perimeter is: "+peri);
+    }
+}
+class rectangle extends shape
+{
+    public void area()
+    {
+        ar = l * b;
+        System.out.println("Area is: " + ar);
+    }
+    public void perimeter()
+    {
+        peri=2*(l+b);
+        System.out.println("Perimeter is: "+peri);
+    }
+}
+public class oops_abstraction
+{
+    public static void main(String[] args)
+    {
+        Scanner ob=new Scanner(System.in);
+        square sq= new square();
+        System.out.print("Enter the length: ");
+        sq.l=ob.nextInt();
+        sq.area();
+        sq.perimeter();
+
+        rectangle re=new rectangle();
+        System.out.print("Enter the length: ");
+        re.l=ob.nextInt();
+        System.out.print("Enter the breadth: ");
+        re.b=ob.nextInt();
+        re.area();
+        re.perimeter();
+    }
+}
